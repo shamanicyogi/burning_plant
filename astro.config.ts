30fc37d1d602f063b507d193001507c555df8d20
Joshua Jones <joshuajonesdev@gmail.com>
2024-03-03 14:17:59 -0500

    Configured Sentry

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,8 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
@@ -15,6 +17,13 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    sentry({
+      dsn: "https://91b2023e87299fcd382ad74bc5eb8330@o4506345239609344.ingest.sentry.io/4506848607272960",
+      sourceMapsUploadOptions: {
+        project: "kimuras_and_bench",
+        // authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
   ],
   markdown: {
     remarkPlugins: [
--- src/pages/api/mailchimp.ts
+++ src/pages/api/mailchimp.ts
@@ -1,42 +1,54 @@
 import type { APIRoute } from "astro";
 import mailchimp from "@mailchimp/mailchimp_marketing";
+import Sentry from "@sentry/node";
+
+Sentry.init({
+  dsn: "https://91b2023e87299fcd382ad74bc5eb8330@o4506345239609344.ingest.sentry.io/4506848607272960",
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  profilesSampleRate: 1.0,
+});
 
 export const POST: APIRoute = async ({ request }) => {
-    const APIKey = import.meta.env.MAILCHIMP_API_KEY;
-    const server = import.meta.env.MAILCHIMP_SERVER;
-    const listId = import.meta.env.MAILCHIMP_LIST_ID;
-    const email = JSON.parse(await request.text()).email;
+  const APIKey = import.meta.env.MAILCHIMP_API_KEY;
+  const server = import.meta.env.MAILCHIMP_SERVER;
+  const listId = import.meta.env.MAILCHIMP_LIST_ID;
+  const email = JSON.parse(await request.text()).email;
 
-    console.log(APIKey, server, email)
+  mailchimp.setConfig({
+    apiKey: APIKey,
+    server: server,
+  });
 
-    mailchimp.setConfig({
-        apiKey: APIKey,
-        server: server,
-    });
+  const subscribingUser = {
+    firstName: "",
+    lastName: "",
+    email,
+  };
 
-    const subscribingUser = {
-        firstName: "",
-        lastName: "",
-        email,
-    };
-
-    async function run() {
-        const response = await mailchimp.lists.addListMember(listId, {
-            email_address: subscribingUser.email,
-            status: "subscribed",
-            merge_fields: {
-            FNAME: subscribingUser.firstName,
-            LNAME: subscribingUser.lastName
-            }
-        });
+  async function run() {
+    try {
+      await mailchimp.lists.addListMember(listId, {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName,
+        },
+      });
+    } catch (error) {
+      Sentry.withScope(scope => {
+        scope.setExtra("Error", "Failed to add subscriber to MailChimp");
+        scope.captureException(error);
+      });
     }
+  }
 
-    run();
+  run();
 
-    return new Response(
-        JSON.stringify({
-        message: "Success!"
-        }),
-        { status: 200 }
-    );
+  return new Response(
+    JSON.stringify({
+      message: "Success!",
+    }),
+    { status: 200 }
+  );
 };
