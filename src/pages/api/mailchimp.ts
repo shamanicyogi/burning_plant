84fe4171f8a43aca8b6f7e121501617a68064f99
Joshua Jones <joshuajonesdev@gmail.com>
2024-03-03 13:41:12 -0500

    successful ping

--- src/pages/api/mailchimp.ts
+++ src/pages/api/mailchimp.ts
@@ -1,9 +1,26 @@
 import type { APIRoute } from "astro";
+import mailchimp from "@mailchimp/mailchimp_marketing";
 
 export const POST: APIRoute = async ({ request }) => {
+    const mailChimpAPIKey = import.meta.env.MAILCHIMP_API_KEY;
+    const mailChimpServer = import.meta.env.MAILCHIMP_SERVER;
     const email = JSON.parse(await request.text()).email;
 
-    console.log(email, 'email')
+    console.log(mailChimpAPIKey, mailChimpServer, email)
+
+    mailchimp.setConfig({
+        apiKey: mailChimpAPIKey,
+        server: mailChimpServer,
+    });
+
+
+    async function run() {
+        const response = await mailchimp.ping.get();
+        console.log(response);
+      }
+      
+      run();
+
 //   const data = await request.formData();
 //   const name = data.get("name");
 //   const email = data.get("email");
@@ -18,8 +35,6 @@ export const POST: APIRoute = async ({ request }) => {
 //     );
 //   }
   // Do something with the data, then return a success response
-
-    console.log('YOOYOs')
 
   return new Response(
     JSON.stringify({
