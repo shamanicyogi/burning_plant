6113d83e663aacdc5d7890a7066787944b45e9b0
Joshua Jones <mrjoshuajones@protonmail.com>
2024-04-30 21:06:07 -0400

    Added image to card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,18 @@
 import { slugifyStr } from "@utils/slugify";
 import Datetime from "./Datetime";
 import type { CollectionEntry } from "astro:content";
+import "./Card.css";
+// import all images for articles
+// no easy way to do dynamic imports
+import jitz from "../assets/images/jitz.jpg";
+import dating from "../assets/images/dating.jpeg";
+import alpha_statue from "../assets/images/alpha_statue.jpeg";
+
+const THUMBNAILS = {
+  jitz,
+  dating,
+  alpha_statue,
+};
 
 export interface Props {
   href?: string;
@@ -9,7 +21,7 @@ export interface Props {
 }
 
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
-  const { title, pubDatetime, modDatetime, description } = frontmatter;
+  const { title, pubDatetime, modDatetime, description, ogImage } = frontmatter;
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
@@ -17,19 +29,22 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
   };
 
   return (
-    <li className="my-6">
-      <a
-        href={href}
-        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
-      >
-        {secHeading ? (
-          <h2 {...headerProps}>{title}</h2>
-        ) : (
-          <h3 {...headerProps}>{title}</h3>
-        )}
-      </a>
-      <Datetime pubDatetime={pubDatetime} modDatetime={modDatetime} />
-      <p>{description}</p>
+    <li className="card-container my-6">
+      <img className="card-image" src={THUMBNAILS[ogImage]?.src} />
+      <div>
+        <a
+          href={href}
+          className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        >
+          {secHeading ? (
+            <h2 {...headerProps}>{title}</h2>
+          ) : (
+            <h3 {...headerProps}>{title}</h3>
+          )}
+        </a>
+        <Datetime pubDatetime={pubDatetime} modDatetime={modDatetime} />
+        <p>{description}</p>
+      </div>
     </li>
   );
 }
