4fb81a7bc74b6d458d1b21f0126990408c4f7904
Joshua Jones <mrjoshuajones@protonmail.com>
2024-04-30 22:56:38 -0400

    commented out eslint

--- eslint.js
+++ eslint.js
@@ -1,10 +1,10 @@
-module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier' // Add Prettier configuration
-  ],
-  rules: {
-    // Add any additional rules or overrides
-  },
-};
+// module.exports = {
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:@typescript-eslint/recommended',
+//     'prettier' // Add Prettier configuration
+//   ],
+//   rules: {
+//     // Add any additional rules or overrides
+//   },
+// };
