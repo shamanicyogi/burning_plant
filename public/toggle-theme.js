a463cdf38021abe987d8b70fe93a3d0bbc6252cb
Joshua Jones <mrjoshuajones@protonmail.com>
2024-05-04 08:52:32 -0400

    TS fixes

--- public/toggle-theme.js
+++ public/toggle-theme.js
@@ -54,6 +54,13 @@ window.onload = () => {
     // set on load so screen readers can get the latest value on the button
     reflectPreference();
 
+    // sync with system changes
+    const prefersDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    themeValue = prefersDarkMode ? "dark" : "light";
+    setPreference();
+
     // now this script can find and listen for clicks on the control
     document.querySelector("#theme-btn")?.addEventListener("click", () => {
       themeValue = themeValue === "light" ? "dark" : "light";
